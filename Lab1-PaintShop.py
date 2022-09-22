bc8d9bc174b766672f0df58cd1601cf7d5d4c8c4
BryGuyZuni <w0469389@nscc.ca>
2022-09-22 13:34:38 -0300

    PaintShop_BryanZuniga

--- Lab1-PaintShop.py
+++ Lab1-PaintShop.py
@@ -13,16 +13,28 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-    print("Welcome to the Paint Shop!")
-    print("Testing")
-    
+    print("Welcome to Bryan's Paint Shop!")
+    print("At BPS, we're here to help you calculate all your painting needs!")
+    print("One gallon of paint costs $14.99. Our prices are the cheapest around!")
+    print("Please enter the size of your flooring in square feet")
 
+    finalGallon = 0.00
 
+    floorInput = input("How many square feet is your floor? *Note* Must follow pattern of: 150, 300, 450, 600, 750, etc)")
+    wallInput = input("How many square feet is ONE of your walls? *Note* Must follow pattern of: 150, 300, 450, 600, 750, etc)")
 
+    floorGallon =int(floorInput) /150
+    wallGallon =int(wallInput) /150 * 2
 
+    trueGallon = int(floorGallon + wallGallon)
 
+    FinalGallon = trueGallon
 
-
-    # YOUR CODE ENDS HERE
-
+    print("You have purchased:")
+    print(FinalGallon, "Gallons of Paint")
+ 
+    TrueTotal = float(FinalGallon * 14.99)
+    print("Your total is:")
+    print("$", TrueTotal)
+    print("Thank you for shopping at Bryan's Paint Shop! We hope to see you again :)")
 main()
